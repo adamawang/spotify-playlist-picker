a63944c4ed31fd1d20017c03ffddd3951bb342ee
Adam Wang <adamw3494@gmail.com>
2017-02-05 19:42:34 -0500

    show correct buttons

--- src/client/playlist.js
+++ src/client/playlist.js
@@ -26,27 +26,33 @@ angular.module('Playlist', [])
 
 .controller('SearchController', function($scope, Search) {
   $scope.data = {};
+  // save playlist data here
   let playlists;
+
+  $scope.showButton = () => playlists === undefined;
+
   $scope.findPlaylist = () => {
     Search.search()
     .then((response) => {
       playlists = response.data.items;
       const randomNum = Math.floor(Math.random() * playlists.length);
       $scope.data.playlist = playlists[randomNum];
-    })
+    });
   };
+
   $scope.pickAnother = () => {
     const nextRandom = Math.floor(Math.random() * playlists.length);
     console.log(playlists[nextRandom])
     $scope.data.playlist = playlists[nextRandom];
-  }
+  };
+
   $scope.showUserData = () => {
     Search.userInfo()
     .then((response) => {
       const userData = response.data
       $scope.data.user = userData;
-    })
-  }
+    });
+  };
 
   $scope.showUserData();
 })
